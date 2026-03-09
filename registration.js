b494aae262b94de29bf6639a2cfb9211b7aef698
StephanieToraldeSGT <toralde.sg@obmontessori.edu.ph>
2026-03-09 22:29:07 +0800

    Update registration.js

--- registration.js
+++ registration.js
@@ -2,19 +2,19 @@ function to_register() {
     let first = document.getElementById('fname').value;
     let last = document.getElementById('lname').value;
     let user = document.getElementById('user').value;
-    let boxeschecked = document.getElementById('terms').checked;
+    let termsChecked = document.getElementById('terms').checked;
 
     if (first === "" || last === "" || user === "") {
         alert("Please fill in all the text boxes!");
         return;
     }
 
-    if (!boxeschecked) {
+    if (!termsChecked) {
         alert("You must agree to the Terms of Use to proceed.");
         return;
     }
 
-    let proceed = confirm("Proceed to register as " + user + "?");
+    let proceed = confirm("Everything looks good! Ready to register as " + user + "?");
     
     if (proceed) {
         window.location.href = "confirmation.html";
